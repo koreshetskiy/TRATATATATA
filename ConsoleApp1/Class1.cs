5edb86cd3288aabe101756600ed1e49b23249059
Евгений <62739149+koreshetskiy@users.noreply.github.com>
2020-04-24 13:25:45 +0700

    Add files via upload

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -10,52 +10,91 @@ namespace GAME
     class Class1
     {
         public bool gameOver = false;
-        int schet = 0;
-        int u;
-        int n;
-        int celX, celY;
-        const int E = 20;
-        const int P = 20;
-        int[] hvostX = new int[1000];// Массив для хвоста со значением X
-        int[] hvostY = new int[1000];// Массив для хвоста со значением У
-        int nomerhv;
+        private int schet = 0, k;
+        private int n;
+        private int u;
+        private int celX, celY;
+        public int S;
+        public int V;
+        private int UR;
+        private int[] hvostX = new int[100];// Массив для хвоста с значением Х
+        private int[] hvostY = new int[100];// Массив для хвоста с значением У, а как без 2 - х массивов сделать, если у меня значения X и Y разные.......
+        private int nomerhv;
         Random R = new Random();
         ConsoleKeyInfo knopki;
-        public bool Setup(bool gameOver)//Переменные 
+        public bool Setup(bool gameOver)// Заданые переменные 
         {
-            gameOver = false;// Игра = ложь
-            u = E / 2 - 1;
-            n = P / 2 - 1;
-            celX = R.Next(3, E - 2);// Рандомное значения фрукты
-            celY = R.Next(3, E - 2);// Рандомное значения фрукты
-            Console.WriteLine();
+            gameOver = false;// Игра закончена = ложь
+            string s = Console.ReadLine();
+            do
+            {
+                if (Int32.TryParse(s, out k))
+                {
+                    if (k == 1 | k == 2 | k == 3)
+                    {
+                        break; // Проверка на введенные данные...............
+                    }
+                    else
+                    {
+                        Console.WriteLine("От 1 до 3");
+                        Setup(gameOver == false);
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Не число");
+                    Setup(gameOver == false);
+                    break;
+                }
+            }
+            while (true);
+            UR = k;
+            if (UR == 1)
+            {
+                S = 10;
+                V = 10;
+            }
+            if (UR == 2)
+            {
+                S = 15;
+                V = 15;
+            }
+            if (UR == 3)
+            {
+                S = 20;
+                V = 20;
+            }
+            n = S / 2 - 1;
+            u = V / 2 - 1;
+            celX = R.Next(3, S - 2);// Рандомное значения фрукты
+            celY = R.Next(3, S - 2);// Рандомное значения фрукты
             return gameOver;// Возращение значения игры
         }
-        public void Draw()//Карта и персонажи
+        public void Draw()// Карта и персонажи
         {
-            if (E == 10 && P == 10)
+            if (S == 10 && V == 10)
                 Thread.Sleep(200);
-            if (E == 15 && P == 15)
+            if (S == 15 && V == 15)
                 Thread.Sleep(100);
-            if (E >= 20 && P >= 20)
+            if (S == 20 && V == 20)
                 Thread.Sleep(50);
             Console.WriteLine();
-            Console.SetCursorPosition(0, 0);// Позиция курсора
-            Console.WriteLine("Управление змейкой осуществляется на стрелочки");
-            for (int i = 0; i < P + 1; i++)
-                Console.Write("%");// Верхняя граница
+            Console.SetCursorPosition(0, 8);// Позиция курсора
+            for (int i = 0; i < V + 1; i++)
+                Console.Write("#");// Верхняя граница
             Console.WriteLine();
 
-            for (int i = 0; i < E; i++)
+            for (int i = 0; i < S; i++)
             {
-                for (int j = 0; j < E; j++)
+                for (int j = 0; j < S; j++)
                 {
-                    if (j == 0 || j == E - 1)
-                        Console.Write("%");// Боковые границы
-                    if (i == n && j == u)
-                        Console.Write("@");// Голова змейки
+                    if (j == 0 || j == S - 1)
+                        Console.Write((char)47);// Боковые границы
+                    if (i == u && j == n)
+                        Console.Write((char)79); // Выдаст O// Голова змейки
                     else if (i == celY && j == celX)
-                        Console.Write("O");// Расположение фрукты
+                        Console.Write((char)70); // Выдаст F// Расположение фрукты
                     else
                     {
                         bool print = false;
@@ -64,7 +103,7 @@ namespace GAME
                             if (hvostX[k] == j && hvostY[k] == i)
                             {
                                 print = true;
-                                Console.Write("o");// Рисование хвоста
+                                Console.Write((char)79); // Выдаст O// Рисование хвоста
                             }
                         }
                         if (!print)
@@ -74,23 +113,23 @@ namespace GAME
                 Console.WriteLine();
             }
 
-            for (int j = 0; j < E + 1; j++)
-                Console.Write("%");// Нижняя граница
+            for (int j = 0; j < S + 1; j++)
+                Console.Write("#");// Нижняя граница
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Баллы: " + schet);
+            Console.WriteLine("Количество баллов: " + schet);
             Console.WriteLine();
-            Console.WriteLine("Задача игры: Ты должен съесть как можно много фрукта 'O'");
-            Console.WriteLine("Выше отображается количество баллов, за каждый съеденный фрукт +10 балла");
+            Console.WriteLine("Задача игры: Вы должны съесть как можно много фрукта 'F'");
+            Console.WriteLine("Выше отображается количество баллов, за каждый съеданный фрукт +10 балла");
         }
-        public void Input_Logic()//ввод логики
+        public void Input_Logic()
         {
-            int predznX = hvostX[0];// Предыдущее значение хвоста
-            int predznY = hvostY[0];
+            int predznX = hvostX[0];// Предудыщий значение хвоста по Х
+            int predznY = hvostY[0];// Предудыщий значение хвоста по Y
             int pred2X;
             int pred2Y;
-            hvostX[0] = u;// Значение хвоста U = значение U
-            hvostY[0] = n;// Значение хвоста N = значение N
+            hvostX[0] = n;// Значение хвоста Х = значение Х
+            hvostY[0] = u;// Значение хвоста У = значение У
             for (int i = 1; i < nomerhv; i++)
             {
                 pred2X = hvostX[i];
@@ -101,71 +140,72 @@ namespace GAME
                 predznY = pred2Y;
             }
         }
-        public void dvijenie()//движение
+        public void dvigenie()
         {
             if (Console.KeyAvailable == true)
             { knopki = Console.ReadKey(true); }
             switch (knopki.Key)
             {
                 case ConsoleKey.UpArrow:
-                    n--;//Вверх
+                    u--;//Вверх
                     break;
 
                 case ConsoleKey.DownArrow:
-                    n++;//Вниз
+                    u++;//Вниз
                     break;
 
                 case ConsoleKey.RightArrow:
-                    u++;//Вправо
+                    n++;//Вправо
                     break;
 
                 case ConsoleKey.LeftArrow:
-                    u--;//Влево
+                    n--;//Влево
                     break;
             }
         }
-        public void result()//результат
+        public void itog()
         {
-            if (u > E)
-                u = 0;
-            else if (u < 0)
-                u = E - 2;// Выход за границы поля и обратное появление с другой стороны
-            if (n > P)
-                n = 0;
-            else if (n < 0)
-                n = P - 2;
+            if (n > S)//
+                n = 0;//
+            else if (n < 0)//
+                n = S - 2;// Выход за границы поля и обратное появление с другой стороны
+            if (u > V)//
+                u = 0;//
+            else if (u < 0)//
+                u = V - 2;//
             for (int g = 0; g < nomerhv; g++)
             {
-                if (hvostX[g] == u && hvostY[g] == n)
+                if (hvostX[g] == n && hvostY[g] == u)
                 {
                     gameOver = true;
-                    Console.WriteLine("GAME OVER!!!");
-                    Console.WriteLine("Выберите и напишите размер поля N x N");
+                    Console.WriteLine("Вы проиграли!!!");
                     Console.WriteLine("Нажмите ENTER");
+                    Thread.Sleep(2000);
+                    Console.ReadKey();
                 }
             }
-            if (gameOver != false)
+            if (gameOver == true)
             {
                 Console.Clear();
                 {
+                    Console.WriteLine("Выберите и введите номер:");
                     Class1 class1 = new Class1();
                     class1.Setup(class1.gameOver);
                     while (!class1.gameOver)
                     {
-                        class1.Draw();//рисование
-                        class1.Input_Logic();//ввод логики
-                        class1.dvijenie();//движение
-                        class1.result();//результат
-                        Thread.Sleep(100);// Замедление консоли
+                        class1.Draw();
+                        class1.Input_Logic();
+                        class1.dvigenie();
+                        class1.itog();
                     }
                     Console.ReadKey(true);
                 }
             }
-            if (u == celX && n == celY)
+            if (n == celX && u == celY)
             {
                 schet += 10;// Суммирование баллов
-                celX = R.Next(5, 17);
-                celY = R.Next(5, 17);
+                celX = R.Next(3, S - 2);
+                celY = R.Next(3, S - 2);
                 nomerhv++;
             }
         }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,21 +8,21 @@ namespace GAME
     {
         static void Main(string[] args)
         {
-Console.WriteLine("Выберите и напишите размер поля N x N");
-            Console.WriteLine("Легкий уровень [поле10х10, скорость медленная]");
-            Console.WriteLine("Средний уровень [поле15х15, скорость нормальная]");
-            Console.WriteLine("Сложный уровень [поле20х20, скорость быстрая]");
+            Console.WriteLine("Управление змейкой происходит на стрелочки");
+            Console.WriteLine("Выберите и введите номер:");
+            Console.WriteLine("Легкий уровень [поле10х10, скорость медленная] - Введите 1");
+            Console.WriteLine("Средний уровень [поле15х15, скорость нормальная] - Введите 2");
+            Console.WriteLine("Сложный уровень [поле20х20, скорость быстрая] - Введите 3");
             Class1 class1 = new Class1();
             class1.Setup(class1.gameOver);
+            while (!class1.gameOver)
             {
-                while (!class1.gameOver)
-                class1.Draw();//рисование
-                class1.Input_Logic();//ввод логики
-                class1.dvijenie();//движение
-                class1.result();//результат
-                Thread.Sleep(100);// Замедление консоли
-            }          
-            Console.ReadKey();
+                class1.Draw();
+                class1.Input_Logic();
+                class1.dvigenie();
+                class1.itog();
+            }
+            Console.ReadLine();
         }
     }
 }
